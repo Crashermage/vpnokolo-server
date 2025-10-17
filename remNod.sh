bc1d3fc19e8a3c0f310d6579a59d98588b52c4a8
Crashermage <49247059+Crashermage@users.noreply.github.com>
2025-10-17 10:42:01 +0300

    Update remNod.sh

--- remNod.sh
+++ remNod.sh
@@ -1,31 +1,35 @@
 apt-get -y update && apt-get -y upgrade
 
- # включаем firewall, но оставляем порт 22 для SSH
- ufw enable && ufw allow OpenSSH
- # настраиваем правило для запрета icmp запросов
- vim /etc/ufw/before.rules 
+# включаем firewall, но оставляем порт 22 для SSH
+ufw enable && ufw allow OpenSSH
+# открываем нужные порты
+ufw allow 443,2222/tcp
  
- # ищем ok icmp code for INPUT  и ok icmp code for FORWARD
- меняем все ACCEPT на DROP
+# настраиваем правило для запрета icmp запросов
+vim /etc/ufw/before.rules 
  
- # в блок for INPUT добавляем:
- -A ufw-before-input -p icmp --icmp-type source-quench -j DROP
+# ищем ok icmp code for INPUT  и ok icmp code for FORWARD
+меняем все ACCEPT на DROP
  
- ufw disable && ufw enable # перезапускаем firefall
+# в блок for INPUT добавляем:
+-A ufw-before-input -p icmp --icmp-type source-quench -j DROP
 
- # устанавливаем docker
- sudo curl -fsSL https://get.docker.com | sh
- # создаем раб.директорию
- mkdir /opt/remnanode && cd /opt/remnanode
- # редактируем файл конфига
- vim .env 
+# перезапускаем firefall
+ufw disable && ufw enable 
+
+# устанавливаем docker
+sudo curl -fsSL https://get.docker.com | sh
+# создаем раб.директорию
+mkdir /opt/remnanode && cd /opt/remnanode
+# редактируем файл конфига
+vim .env 
 
 # вставляем 
 APP_PORT=2222
 SSL_CERT=# переходим в панель и добавляя новую ноду копируем сертификат
 
 # создаем docker compose
-nano docker-compose.yml
+vim docker-compose.yml
 
 # вставляем содержимое
 services:
@@ -40,7 +44,6 @@ services:
  
  docker compose up -d && docker compose logs -f -t # запускаем нашу ноду
 
-# открываем порт для vless
- ufw allow 443/tcp
+
  
  
